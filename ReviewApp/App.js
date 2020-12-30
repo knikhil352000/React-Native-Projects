5530c7d8bc094f02d5ca302c13a093ac2187d199
Kumar Nikhil <knikhil352000@gmail.com>
2020-12-31 01:06:27 +0530

    Custom Fonts in React Native

--- ReviewApp/App.js
+++ ReviewApp/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home';
+import * as Font from 'expo-font';
+
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  useEffect(() => {
+    setFontsLoaded(true);
+    getFonts();
+  })
   return (
-    <View>
+    <View> 
       <Home /> 
-    </View>
+    </View> 
   );
 }
--- ReviewApp/screens/Home.js
+++ ReviewApp/screens/Home.js
@@ -4,13 +4,18 @@ import { View, Text, StyleSheet } from 'react-native'
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Text>Home Screen</Text>
+            <Text style={styles.titleText}>Home Screen</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 24
+        padding: 100,
+    },
+    titleText:{
+        fontFamily:'nunito-bold',
+        fontSize:18
+
     }
 })
