0c5d134533622f651346f3509dfc63fc2dd85d3b
Kumar Nikhil <knikhil352000@gmail.com>
2021-01-19 02:45:01 +0530

    Adding Firebase for signUp

--- Instagram/App.js
+++ Instagram/App.js
@@ -5,16 +5,27 @@ import {NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen from './components/auth/Landing';
 import Register from './components/auth/Register';
-
+require('dotenv').config()
+import * as firebase from 'firebase';
 const Stack = createStackNavigator();
-
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+if(firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Landing'>
-        <Stack.Screen name='Landing' component={Register} options={{ headerShown: false}}/>
+        <Stack.Screen name='Landing' component={LandingScreen} options={{ headerShown: false}}/>
 
       </Stack.Navigator>
     </NavigationContainer>
   ); 
-}
+}  
--- Instagram/components/auth/Register.js
+++ Instagram/components/auth/Register.js
@@ -1,13 +1,19 @@
 import React,{useState} from 'react'
 import { Button, View } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler';
-
+import firebase from 'firebase'
 export default function Register() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
     const onSignUp = () => {
-        console.log(email);
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
     
     return (
