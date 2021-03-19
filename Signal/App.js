0d88c1a2f16b83b93bf0d9fd3bfd44c291600b2b
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-20 02:09:49 +0530

    Setting up navigation and firebase

--- Signal/App.js
+++ Signal/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler'
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
---
+++ Signal/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCsUq0aSJTviRMAup8ZGk6DTYUL1FV65s0",
+    authDomain: "signal-clone-9678b.firebaseapp.com",
+    projectId: "signal-clone-9678b",
+    storageBucket: "signal-clone-9678b.appspot.com",
+    messagingSenderId: "179931676753",
+    appId: "1:179931676753:web:89eec84c751f40da92b870"
+  };
