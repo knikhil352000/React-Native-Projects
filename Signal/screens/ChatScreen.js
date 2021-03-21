b37028a491c6c4f5327cbf3763cc156bf1749d31
Kumar Nikhil <knikhil352000@gmail.com>
2021-03-21 16:44:07 +0530

    Added header to the chatscreen

--- Signal/screens/ChatScreen.js
+++ Signal/screens/ChatScreen.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-const ChatScreen = ({ navigation }) => {
+import React, { useLayoutEffect } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { Avatar } from 'react-native-elements'
+import { AntDesign } from '@expo/vector-icons'
+import { FontAwesome } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'; 
+const ChatScreen = ({ navigation, route }) => {
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerTitleAlign: 'left',
+            headerTitle: () => (
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar rounded source={{uri: 'https://avatars.githubusercontent.com/u/71940958?s=400&u=24ef1c10f91e5ef2dffac627465b825a9fef769a&v=4'}}/>
+                    <Text style={{color: 'white', marginLeft: 10, fontWeight: '700'}}>{route.params.chatName}</Text>
+                </View>
+            ),
+            headerLeft: () => (
+                <TouchableOpacity style={{ marginLeft: 10 }} onPress={navigation.goBack}>
+                    <AntDesign name='arrowleft' size={24} color='white'/>
+                </TouchableOpacity>
+            ),
+            headerRight: () => (
+                <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    width: 80,
+                    marginRight: 20,
+                }}>
+                    <TouchableOpacity>
+                        <FontAwesome name='video-camera' size={24} color='white'/>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name='call' size={24} color='white'/>
+                    </TouchableOpacity>
+                </View>
+            )
+        })
+    }, [navigation])
     return (
         <View>
-            <Text></Text>
+            <Text>{route.params.chatName}</Text>
         </View>
     )
 }
